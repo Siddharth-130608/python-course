e7f0b1ac3a5e34912d26276b3dff7bb02d5e9df5
siddharth gupta <mranirudh05@gmail.com>
2025-06-20 18:57:38 +0530

    string methods

---
+++ 08_string method/method.py
@@ -0,0 +1,7 @@
+text="    hello world     "
+print(text.strip())
+print(text.lstrip())
+print(text.find("l"))
+print(text.replace("l","AA"))
+print(text.split("@"))
+print(",".join(['    hello world     ']))
